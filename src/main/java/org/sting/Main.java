57f3faf31e60369ce6b216ff0c55ba8d9571b815
Altynai <salamatovaaltynai50@gmail.com>
2026-07-20 21:03:18 +0600

    Добавлено решение для задачи sumDouble

--- src/main/java/org/sting/Main.java
+++ src/main/java/org/sting/Main.java
@@ -2,9 +2,14 @@ package org.sting;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public boolean sameFirstLast(int[] nums) {
-        return nums.length >= 1 && nums[0] == nums[nums.length - 1];
-
+public class Main {public int sumDouble(int a, int b) {
+    if (a == b){
+        return (a + b) * 2;
     }
+    return a + b;
+
+
+
+}
+
 }
