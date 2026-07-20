f9f96749c798cae3f1b565ef7e53a4fc0d94993b
Altynai <salamatovaaltynai50@gmail.com>
2026-07-20 21:44:20 +0600

    Добавлено решение для задачи diff21

--- src/main/java/org/sting/Main.java
+++ src/main/java/org/sting/Main.java
@@ -1,15 +1,15 @@
 package org.sting;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {public int sumDouble(int a, int b) {
-    if (a == b){
-        return (a + b) * 2;
+public class Main {public int diff21(int n) {
+    if (n > 21){
+        return (n - 21) * 2;
     }
-    return a + b;
+    return 21 - n;
+
+}
+
 
 
 
 }
 
-}
